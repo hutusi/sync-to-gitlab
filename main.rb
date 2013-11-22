95dc4f458c48953b1c9215cd18e840fecdcc35be
John Hu <huziyong@gmail.com>
2013-11-22 21:03:42 +0800

    main for init_all and sync_all

--- main.rb
+++ main.rb
@@ -2,8 +2,30 @@
 require 'repository'
 require 'json'
 
-config = JSON.parse(File.read("config.json"))
-workspace = config["workspace"]
-init_repos = config["init"]
+def parse_config
+  JSON.parse(File.read("config.json"))
+end
 
+def init_all
+  config = parse_config
+  workspace = config["workspace"]
+  repos = config["init"]
+
+  repos.each { |x|
+    path = File.join(workspace, x["name"] + ".git")
+    repo = Repository.new(path, x["src_url"], x["dest_url"])
+    repo.init
+  }
+end
+
+def sync_all
+  config = parse_config
+  workspace = config["workspace"]
+
+  path = File.join(workspace, "*.git")
+  Dir.glob(path) { |x|
+    repo = Repository.new(path)
+    repo.sync
+  }
+end
 
