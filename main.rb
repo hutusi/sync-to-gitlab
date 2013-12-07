a7ca67a018bf837f5b532e1db21b2ce9657d7924
John Hu <huziyong@gmail.com>
2013-12-07 11:24:06 +0800

    read form new form of config

--- main.rb
+++ main.rb
@@ -1,6 +1,53 @@
 
-require 'repository'
+# require 'repository'
 require 'json'
+
+class Synchronizer
+  def parse
+    @config = {}
+    @config["github"] = "http://github.com/"
+    @config["gitlab"] = "git@gitlab.local:"
+    projects_config = JSON.parse(File.read("projects.json"))
+    @workspaces = parse_workspaces projects_config
+    p @workspaces
+  end
+
+  def create_all
+  end
+
+  def init_all
+  end
+
+  def sync_all
+  end
+
+  private
+    def parse_workspaces(config)
+      workspaces = []
+      config.each {|w|
+        ws = {}
+        ws["dir"] = w["workspace"]
+        group = w["group"]
+        ws["group"] = group
+        projects = []
+        w["projects"].each {|p|
+          p.strip!
+          name = p.split('/')[1]
+          prj = {}
+          prj["name"] = name
+          prj["github"] = "#{@config["github"]}#{p}.git"
+          prj["gitlab"] = "#{@config["gitlab"]}#{group}/#{name}.git"
+          projects << prj
+        }
+        ws["projects"] = projects
+        workspaces << ws
+      }
+      workspaces
+    end
+
+    def create
+    end
+end
 
 def parse_config
   JSON.parse(File.read("config.json"))
@@ -29,3 +76,5 @@ def sync_all
   }
 end
 
+sync = Synchronizer.new 
+sync.parse
--- project.rb
+++ project.rb
@@ -26,30 +26,6 @@ class Project
     resp = Net::HTTP.get_response(URI.parse(url))
     resp.body
   end
-
-  def list
-    url = URI("http://gitlab.local/api/v3/projects?private_token=gyhzxARwhpt23Y8Qogbg")
-    resp = Net::HTTP.get_response(URI.parse(url))
-    
-    resp_text = resp.body
-    p resp_text 
-  end
-
-  def create(project_name)
-    url = URI("http://gitlab.local/api/v3/projects?private_token=gyhzxARwhpt23Y8Qogbg")
-    params = {
-      "name" => project_name,
-      "description" => '',
-      "issues_enabled" => false,
-      "wall_enabled" => false,
-      "merge_requests_enabled" => false,
-      "wiki_enabled" => false,
-      "snippets_enabled" => false,
-      "public" => true
-    }
-
-    resp = Net::HTTP.post_form(url, params)
-  end
   
   def create_project_for_group(group_id, project_name)
     url = "#{projects_url}&namespace_id=#{group_id}"
